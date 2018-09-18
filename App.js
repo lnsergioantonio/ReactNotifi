9fd969e788eb3818da3ab5041e704dbb6be5654f
sergio <lnsergioantonio@gmail.com>
2018-09-18 10:24:48 -0500

    Fix Notifications

--- App.js
+++ App.js
@@ -74,17 +74,6 @@ export default class App extends Component{
                     <Text style={styles.instructions}>2.- Al recibir el login se guarda los datos en Firestore</Text>
                     <Text style={styles.instructions}>3.- Si estas logeado recibes una notifiación de que alguien inicio sesión</Text>
 
-                    <TouchableOpacity style={styles.button} onPress={() => { this.notif.localNotif() }}><Text>Local Notification (now)</Text></TouchableOpacity>
-                    <TouchableOpacity style={styles.button} onPress={() => { this.notif.scheduleNotif() }}><Text>Schedule Notification in 30s</Text></TouchableOpacity>
-                    <TouchableOpacity style={styles.button} onPress={() => { this.notif.cancelNotif() }}><Text>Cancel last notification (if any)</Text></TouchableOpacity>
-                    <TouchableOpacity style={styles.button} onPress={() => { this.notif.cancelAll() }}><Text>Cancel all notifications</Text></TouchableOpacity>
-                    <TouchableOpacity style={styles.button} onPress={() => { this.notif.checkPermission(this.handlePerm.bind(this)) }}><Text>Check Permission</Text></TouchableOpacity>
-
-                    <View style={styles.spacer}></View>
-                            <TextInput style={styles.textField} value={this.state.senderId} onChangeText={(e) => {this.setState({ senderId: e })}} placeholder="GCM ID" />
-                            <TouchableOpacity style={styles.button} onPress={() => { this.notif.configure(this.onRegister.bind(this), this.onNotif.bind(this), this.state.senderId) }}><Text>Configure Sender ID</Text></TouchableOpacity>
-                            {this.state.gcmRegistered && <Text>GCM Configured !</Text>}
-
                     <View style={styles.spacer}></View>
                     {
                         !this.state.isLoginScreenPresented ?
@@ -196,8 +185,8 @@ export default class App extends Component{
     }
     onNotif(notif) {
         console.log(notif);
-        const { foreground, userInteraction, message, data, ...rest } = notif;
-        Alert.alert(notif.title, notif.message);
+        // const { foreground, userInteraction, message, data, ...rest } = notif;
+        // Alert.alert(notif.title, notif.message);
     }
     handlePerm(perms) {
         Alert.alert("Permissions", JSON.stringify(perms));
@@ -214,29 +203,5 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         color: '#333333',
         marginBottom: 5,
-    },
-    button: {
-        borderWidth: 1,
-        borderColor: "#000000",
-        margin: 5,
-        padding: 5,
-        width: "70%",
-        backgroundColor: "#DDDDDD",
-        borderRadius: 5,
-    },
-    textField: {
-        borderWidth: 1,
-        borderColor: "#AAAAAA",
-        margin: 5,
-        padding: 5,
-        width: "70%"
-      },
-      spacer: {
-        height: 10,
-      },
-      title: {
-        fontWeight: "bold",
-        fontSize: 20,
-        textAlign: "center",
     }
 });
